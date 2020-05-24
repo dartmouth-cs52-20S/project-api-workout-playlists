a373e98ef62c673f1f97fc991ec96df69780d0f2
Catherine Parnell <catherine.d.parnell.22@dartmouth.edu>
2020-05-24 18:42:38 -0500

    eslint started working woo

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import spotifyCredentials from 'router.js';
+// import spotifyCredentials from 'secrets.js';
+import * as UserController from './controllers/user_controller';
 
 const router = Router();
 
@@ -7,25 +8,24 @@ router.get('/', (req, res) => {
   res.json({ message: 'Hello World' });
 });
 
-// comments will be needed in deployment
-router.get('/spotify-credentials', (req, res, next) => {
-  // const clientId = process.env.clientId;
-  // const clientSecret = process.env.clientSecret;
-  // const redirectUri = process.env.redirectUri;
-  // const spotifyCredentials = { clientId, clientSecret, redirectUri };
-  res.json(spotifyCredentials);
-});
+// router.get('/spotify-credentials', (req, res, next) => {
+//   // const clientId = process.env.clientId;
+//   // const clientSecret = process.env.clientSecret;
+//   // const redirectUri = process.env.redirectUri;
+//   // const spotifyCredentials = { clientId, clientSecret, redirectUri };
+//   res.json(spotifyCredentials);
+// });
 
-router.route('/authorize')
-  .put(setUser);
+// router.route('/authorize')
+//   .put(UserController.setUser);
 
-router.route('/preferences')
-  .put(userPreferences);
+// router.route('/preferences')
+//   .put(UserController.userPreferences);
 
 router.route('/newuser')
-  .post(startUser);
+  .post(UserController.startUser);
 
 router.route('/getuser')
-  .get(getUser);
+  .get(UserController.getUser);
 
 export default router;
